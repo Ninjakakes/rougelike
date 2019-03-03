b75dc7a0746be1ed801c731f2f4547779aba0198
Ninjakakes <ninjakakes@gmail.com>
2019-03-03 16:27:50 -0500

    Defined console dimensions

--- Rougelike/Game.cs
+++ Rougelike/Game.cs
@@ -5,10 +5,29 @@ namespace Rougelike
     class Game
     {
         // The screen height and width are in number of tiles
-        private static readonly int _screenwidth = 100;
-        private static readonly int _screenheight = 70;
-
+        private static readonly int _screenWidth = 100;
+        private static readonly int _screenHeight = 70;
         private static RLRootConsole _rootConsole;
+
+        // The map console takes up most of the screen and is where the map will be drawn
+        private static readonly int _mapWidth = 80;
+        private static readonly int _mapHeight = 48;
+        private static RLConsole _mapConsole;
+
+        // Below the map console is the message console which displays attack rolls and other information
+        private static readonly int _messageWidth = 80;
+        private static readonly int _messageHeight = 11;
+        private static RLConsole _messageConsole;
+
+        // The stat console is to the right of the map and display player and monster stats
+        private static readonly int _statWidth = 20;
+        private static readonly int _statHeight = 70;
+        private static RLConsole _statConsole;
+
+        // Above the map is the inventory console which shows the players equipment, abilities, and items
+        private static readonly int _inventoryWidth = 80;
+        private static readonly int _inventoryHeight = 11;
+        private static RLConsole _inventoryConsole;
 
         static void Main()
         {
@@ -17,7 +36,7 @@ namespace Rougelike
             // The title will appear at the top of the console window
             string consoleTitle = "RougeSharp V3 Tutorial - Level 1";
             // Tell RLNet to use the bitmap font that we specified and that each tile is 8 x 8 pixels
-            _rootConsole = new RLRootConsole(fontFileName, _screenwidth, _screenheight,
+            _rootConsole = new RLRootConsole(fontFileName, _screenWidth, _screenHeight,
                 8, 8, 1f, consoleTitle);
             // Set up a handler for RLNET's Update event
             _rootConsole.Update += OnRootConsoleUpdate;
