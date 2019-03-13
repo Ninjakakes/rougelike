67c6ee6c80d7f5ce0b07d8bafc0c8b32f7c7ad1b
Ninjakakes <ninjakakes@gmail.com>
2019-03-13 12:25:41 -0400

    Added the Player class

--- Rougelike/Core/Colors.cs
+++ Rougelike/Core/Colors.cs
@@ -15,5 +15,7 @@ namespace Rougelike.Core
         public static RLColor WallFov = Swatch.SecondaryLighter;
 
         public static RLColor TextHeading = Swatch.DbLight;
+
+        public static RLColor Player = Swatch.DbLight;
     }
 }
