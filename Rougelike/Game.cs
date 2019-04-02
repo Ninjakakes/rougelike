f679a56ca903da35e8562833523552aac057d745
Ninjakakes <ninjakakes@gmail.com>
2019-04-02 14:00:25 -0400

    Completed RogueSharp V3 Tutorial – Monster Stats

--- Rougelike/Game.cs
+++ Rougelike/Game.cs
@@ -135,7 +135,11 @@ namespace Rougelike
         {
             if (_renderRequired)
             {
-                DungeonMap.Draw(_mapConsole);
+                _mapConsole.Clear();
+                _statConsole.Clear();
+                _messageConsole.Clear();
+
+                DungeonMap.Draw(_mapConsole, _statConsole);
                 Player.Draw(_mapConsole, DungeonMap);
                 Player.DrawStats(_statConsole);
                 MessageLog.Draw(_messageConsole);
